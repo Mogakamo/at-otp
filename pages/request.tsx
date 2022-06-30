dc0f9a34facf4876ad96be8a0530c6ac71611819
unknown <antonymurugu@outlook.com>
2022-06-30 14:54:31 +0300

    verify page

--- pages/request.tsx
+++ pages/request.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const requestOTP = () => {
+  const [checked, setChecked] = useState(false);
+  
+  useEffect(() =>{
+
+  });
+const  submitHandler=()=>{
+
+}
+
   return (
     <div className=' flex items-center justify-center bg-slate-100 h-screen' >
-      <form className='flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96'>
+      <form onSubmit={submitHandler} className='flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96'>
         <h1>Choose an option to receive your OTP</h1>
-        <input type="checkbox" name="" id="" />Telegram
-        <input type="checkbox" name="" id="" />Whatsapp
+        <div className='mx-10 flex-col space-y-5 flex  pt-4'>
+        <input className='mx-15 flex-row ' type="checkbox" name="" id="" />Telegram
+        <input className='mx-15' type="checkbox" name="" id="" />Whatsapp
+        <input className='mx-15' type="checkbox" name="" id="" />Messenger
+       </div>
+
+       <div className='pt-10'>
+       <button className='bg-green-500 p-5 rounded-md' type = "submit"> Save </button>
+       </div>
+        
         
       </form>
     </div>
   )
 }
 
-export default requestOTP
+export default requestOTP;
--- pages/verify.tsx
+++ pages/verify.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+
+
 
 const Verify = () => {
   return (
-    <div>Verify</div>
+    <div className='flex items-center justify-center bg-slate-100 h-screen'>
+     <form  className='flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96'>
+           <h1>Please Verify your One Time Password</h1>
+      </form>
+
+
+    </div>
   )
 }
 
