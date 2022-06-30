2cb6491190b06df631230f7faa1b280b92022af8
Mogakamo <mogaka.amo254@gmail.com>
2022-06-30 14:48:05 +0300

    updates

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,60 @@
-import React from 'react'
+import React from "react";
+import { phone } from "phone";
+import Link from "next/link";
 
 const Login = () => {
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if(phoneNumber){
+    let pn = phone(phoneNumber,{country: 'KE'});
+    console.log({pn,phoneNumber})
+
+    if (pn.isValid) {
+      // phone is valid;
+      let validPhoneNumber = pn.phoneNumber;
+      // you can send the phone number to your server for verification
+      alert(`Phone number is valid: ${validPhoneNumber}`);  
+    } else {
+      alert("Invalid phone number");
+    }}
+  };
+
+  const handleChange = () => {};
+
   return (
-    <div className=' flex items-center justify-center bg-slate-100 h-screen' >
-      <form className='flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96'>
+    <div className=" flex items-center justify-center bg-slate-100 h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96"
+      >
         <h1>Input your phone number</h1>
-        <div className='border-black border-2'>
-        <input type="tel" name="phone_number" id="" className='' />
+        <div className="border-black border-2">
+          <div>
+
+          </div>
+          <input
+            onChange={(element) => {
+              let tel = element.target.value;
+              setPhoneNumber(tel);
+            }}
+            type="tel"
+            name="phone_number"
+            id=""
+            className=""
+          />
         </div>
+        <input type="password" name="password" id="" />
+        <button
+          className="px-10 py-6 rounded-lg border-black text-white bg-green-500"
+          onClick={handleChange}
+        >
+         <Link href="/request"> Login</Link>
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
