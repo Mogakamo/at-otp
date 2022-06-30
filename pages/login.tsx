2a1d495b6934ff2922f52922bc3f7a41a51e670a
Mogakamo <mogaka.amo254@gmail.com>
2022-06-30 13:33:04 +0300

    Added the phone package

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className=' flex items-center justify-center bg-slate-100 h-screen' >
+      <form className='flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96'>
+        <h1>Input your phone number</h1>
+        <div className='border-black border-2'>
+        <input type="tel" name="phone_number" id="" className='' />
+        </div>
+      </form>
+    </div>
   )
 }
 
