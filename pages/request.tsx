86b4602083a670ed87cb8fcd51e7270f995585eb
Mogakamo <mogaka.amo254@gmail.com>
2022-06-30 13:27:57 +0300

    request form

--- pages/request.tsx
+++ pages/request.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const requestOTP = () => {
   return (
-    <div>requestOTP</div>
+    <div className=' flex items-center justify-center bg-slate-100 h-screen' >
+      <form className='flex p-10 flex-col bg-white rounded-md items-center shadow-lg h-96 w-96'>
+        <h1>Choose an option to receive your OTP</h1>
+        <input type="checkbox" name="" id="" />Telegram
+        <input type="checkbox" name="" id="" />Whatsapp
+        
+      </form>
+    </div>
   )
 }
 
